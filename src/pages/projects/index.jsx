cb96361ef975454779ff108f83519bed8051de43
adcavalcant <aderc19@gmail.com>
2023-11-21 19:22:42 -0300

    Alteração da URL para tornar clicável

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,44 +1,57 @@
-import React, { useEffect, useState } from 'react'
-import { Container, Content, Ul, Li, TitleProject, Url, Created_at } from '../../styles/projects/style'
+import React, { useEffect, useState } from "react";
+import {
+  Container,
+  Content,
+  Ul,
+  Li,
+  TitleProject,
+  Url,
+  Created_at,
+} from "../../styles/projects/style";
 
 export default function Projects() {
-  const [itemsApi, setItemsApi] = useState([])
+  const [itemsApi, setItemsApi] = useState([]);
 
   useEffect(() => {
-    let abortController = new AbortController(); 
+    let abortController = new AbortController();
 
     function getGitHubAPI() {
-      fetch('https://api.github.com/users/adcavalcant/repos')
-      .then(async res => {
-        if (!res.ok) {
-          throw new Error(res.status)
-        }
-        var data = await res.json()
-        setItemsApi(data)
-      })
-      .catch(e => console.log(e))
+      fetch("https://api.github.com/users/adcavalcant/repos")
+        .then(async (res) => {
+          if (!res.ok) {
+            throw new Error(res.status);
+          }
+          var data = await res.json();
+          setItemsApi(data);
+        })
+        .catch((e) => console.log(e));
     }
 
-    getGitHubAPI()
+    getGitHubAPI();
 
-    return () => abortController.abort();  
-  }, [])
+    return () => abortController.abort();
+  }, []);
 
   return (
     <Container>
-     <Content>
-       <Ul>
-        {itemsApi.map(item => (
-          <Li key={item.id}>
-            <TitleProject>{item.name.toUpperCase()}</TitleProject>
-            <Url>URL: {item.html_url}</Url>
-            <Created_at>Data Criação: { Intl.DateTimeFormat('pt-BR')
-              .format(new Date(item.created_at))}
-            </Created_at>
-          </Li>
-        ))}
-       </Ul>
-     </Content>
-   </Container>
-  )
+      <Content>
+        <Ul>
+          {itemsApi.map((item) => (
+            <Li key={item.id}>
+              <TitleProject>{item.name.toUpperCase()}</TitleProject>
+              <Url>
+                <a href={item.html_url} target="_blank">
+                  {item.html_url}
+                </a>
+              </Url>
+              <Created_at>
+                Data Criação:{" "}
+                {Intl.DateTimeFormat("pt-BR").format(new Date(item.created_at))}
+              </Created_at>
+            </Li>
+          ))}
+        </Ul>
+      </Content>
+    </Container>
+  );
 }
